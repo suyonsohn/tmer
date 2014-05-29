3ed2efc6d01776e419f87f3c15d7c9201e666bfc
CMClay <colinclay@gmail.com>
2014-05-29 20:49:24 +0000

    login working

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,29 @@
 # Homepage (Root path)
+helpers do
+  # Usable in ERB templates everywhere, and in any action below
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+end
+
 get '/' do
   erb :index
+end
+
+#Login ===========================================
+
+post '/' do
+  @user = User.find_by(email: params[:email], password: params[:password])
+
+  if @user
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :'auth/login'
+  end
+end
+
+post '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
